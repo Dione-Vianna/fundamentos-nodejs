c4702c7f535bbba4bc361d4961d4e8ffb8e6e14c
Dione Vianna <dioneg12@hotmail.com>
2024-09-22 12:52:41 -0300

    :zap: Rotas com parâmetros (RegExp)

--- src/server.js
+++ src/server.js
@@ -42,10 +42,11 @@ const server = http.createServer(async (request, response) => {
   await json(request, response)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if(route){
+    const routeParams = request.url.match(route.path)
     return route.handle(request, response)
   }
  
