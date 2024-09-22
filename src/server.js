cb00409bc1dec09d3db585cb0c0d8597275d7d48
Dione Vianna <dioneg12@hotmail.com>
2024-09-22 11:39:04 -0300

    :zap: Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 // - HTTP METHODS
 // GET => Buscar uma informação do back-end
@@ -26,23 +27,11 @@ const users = []
 const server = http.createServer(async (request, response) => {
   const {method, url} = request
 
-
-  const buffers = []
-
-  for await(const chunk of request) {
-    buffers.push(chunk)
-  }
-
-  try{
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    request.body = null
-  }
+  await json(request, response)
 
   if (method === "GET" && url === "/users") {
 
     return response
-    .setHeader('Content-type', 'application/json')
     .end(JSON.stringify(users)) // JSON - javaScript Object Notation
   }
 
